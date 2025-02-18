7a0b8776d2b671eb82b38f544fbc806c5f23d88b
Florian Fourcade <florian.fourcade.ext@orange.com>
2025-02-18 14:35:28 +0100

    Initial Commit

---
+++ nutrition-app/nutrition_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  nutrition_appApp.swift
+//  nutrition-app
+//
+//  Created by Florian Fourcade on 18/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct nutrition_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
